e8f12d4d7a5527a8d07341164a2df7617060cc5d
Michael Kofoed <miko@imac.local>
2022-08-24 14:39:08 +0200

    update examples

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # asyncapi-examples
+
+# update-examples
+
+remove all examples manuel and run
+
+´% ./update-examples.sh
+
+git add -A && git commit -m "update examples"
---
+++ update-examples.sh
@@ -0,0 +1,17 @@
+ag \
+-o asyncapi-catalog-provider \
+-p springBootVersion=2.7.4-SNAPSHOT \
+-p springCloudStreamVersion=2021.0.3 \
+-p springCloudVersion=2021.0.3 \
+-p view=provider \
+https://raw.githubusercontent.com/finetech-dk/specs/main/asyncapi-catalog.yaml \
+@asyncapi/java-spring-cloud-stream-template
+
+ag \
+-o asyncapi-catalog-client \
+-p springBootVersion=2.7.4-SNAPSHOT \
+-p springCloudStreamVersion=2021.0.3 \
+-p springCloudVersion=2021.0.3 \
+-p view=client \
+https://raw.githubusercontent.com/finetech-dk/specs/main/asyncapi-catalog.yaml \
+@asyncapi/java-spring-cloud-stream-template
